858ca2d00752fcb1d7703e04241c261a69eac51f
Nasreen-3455 <nasreentajmj498@gmail.com>
2026-02-24 15:39:30 +0530

    added root route

--- server.js
+++ server.js
@@ -2,21 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
+
 const app = express();
+
+// Middlewares
 app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
+
+// Routes
 const authRoutes = require("./routes/authRoutes");
 const noteRoutes = require("./routes/noteRoutes");
-
-
-app.use(express.json()); // VERY IMPORTANT
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch(err => console.log("MongoDB Error:", err));
+
+// ✅ IMPORTANT FOR RENDER
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
